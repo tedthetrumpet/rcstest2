2c486c90e6580e29c6b18e3770c266ad09dce2fd
tedthetrumpet <tedthetrumpet@gmail.com>
2024-08-26 17:23:55 +0100

    whatever

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Not my site',
-  tagline: 'Again',
+  tagline: 'Still trying',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
